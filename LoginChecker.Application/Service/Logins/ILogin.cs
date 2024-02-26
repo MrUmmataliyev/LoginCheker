9ad5066be1b37eb2f8223217bb45fdaf3d5a7ef5
MrUmmataliyev <mr.umataliev.1817@gmail.com>
2024-02-26 18:40:25 +0500

    Added services with classes and interfaces

---
+++ LoginChecker.Application/Service/Logins/ILogin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginChecker.Application.Service.Logins
+{
+    public interface ILogin
+    {
+    }
+}
